1f72c478aa04c1e7f875bb84ba2b12fbf91ee0a1
tinohertlein <tino@hertlein.dev>
2023-02-02 08:08:46 +0100

    feat: visualize sleep time

--- src/main/kotlin/dev/hertlein/logging/Calculator.kt
+++ src/main/kotlin/dev/hertlein/logging/Calculator.kt
@@ -2,13 +2,23 @@ package dev.hertlein.logging
 
 import java.util.concurrent.TimeUnit
 
+private const val THE_ANSWER = 42
+private const val SLEEP_DURATION = 5
+
 object Calculator {
 
-    fun calculateHeavily(): Int {
-        val result = 42
-        println("----- Computing heavily -----")
-        TimeUnit.SECONDS.sleep(5)
-        return result
+    fun calculateHeavily(duration: Int = SLEEP_DURATION): Int {
+        println("----- Start computing heavily -----")
+
+        repeat(duration) {
+            print(".")
+            TimeUnit.SECONDS.sleep(1)
+        }
+
+        println()
+        println("----- End computing heavily -----")
+
+        return THE_ANSWER
     }
 
     fun calculateWithException() {
